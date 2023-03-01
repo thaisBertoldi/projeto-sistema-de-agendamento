47772d9c41fb67b526e6555e2483efda3534df53
Thais B <thais_brt@hotmail.com>
2023-02-28 21:14:31 -0300

    init: Projeto iniciado

---
+++ api-node-mongodb/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/agendamento');
+
+app.get('/', (req, res) => {
+    res.send('Olá, teste!');
+});
+
+app.listen(8080, () => {});
---
+++ react-sistema-agendamento/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testando</h1>
+    </div>
+  );
+}
+
+export default App;
